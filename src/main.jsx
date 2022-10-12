897fbaf9edbc62878a035b1d69a04e198ac90592
Ciro Batista da Silva <cirobatista.cbs2020ti@outlook.com>
2022-10-12 20:25:00 -0300

    Revert "EsLint Config Update"
    This reverts commit 5aedf173e3eb5422d2363831d1a49d981646f641.

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './styles/global.css'
 
 // import Home from './pages/Home/index';
-// Quando o 'export' é adicionado na função e não no final como 'default' ele é sinalizado na importação entre 'chaves'.
+// Quando o 'export' é adionado na função e não no final como 'default' ele é sinalizado na importação entre 'chaves'.
 // When 'export' is added in the function and not at the end as 'default' it is flagged on import between 'braces'.
 
 import { Home } from './pages/Home/index.jsx'
